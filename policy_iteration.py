a589e85dc6b291c4c67da56684e251e669ae8c5f
JIANI YE <jiani.ye@ligo.org>
2020-08-02 19:00:03 -0400

    policy based iteration

--- policy_iteration.py
+++ policy_iteration.py
@@ -1,9 +1,9 @@
 import numpy as np 
-import rl_gridworld as gw
+import gridworld as gw
 
 def print_policy(P, g):
     r, c = g.rows, g.columns
-    Ps = [[{} for j in range(c)] for i in range(r)]
+    Ps = [[0 for j in range(c)] for i in range(r)]
     poses = list(P.keys())
     for pos in poses:
         Ps[pos[0]][pos[1]] = P.get(pos)
@@ -19,7 +19,7 @@ def print_value(V, g):
         Vs[pos[0]][pos[1]] = V.get(pos)
     print(Vs)
 
-def policy_evaluation(policy_prob, g, gamma, delta):
+def policy_evaluation(policy, g, gamma, delta):
     V = {}
     for s in g.all_states():
         V[s] = 0
@@ -30,28 +30,28 @@ def policy_evaluation(policy_prob, g, gamma, delta):
         itermax = 0
         V_ = {}
         for s in g.all_states():
-            actions = g.actions.get(s, ())
+            a = policy.get(s, 0)
             V_[s] = 0
-            for a in actions:
-                probs = g.probs.get((s,a), {})
-                for s_ in list(probs.keys()):
-                    r = g.rewards.get(s_, 0)
-                    V_[s] += policy_prob.get(s, {}).get(a, 0)*probs[s_]*(r + gamma*V[s_])
+            probs = g.probs.get((s,a), {})
+            for s_ in list(probs.keys()):
+                r = g.rewards.get(s_, 0)
+                V_[s] += probs[s_]*(r + gamma*V[s_])
             itermax = max(itermax, abs(V_[s]-V[s]))
         if itermax < delta: break
         V = V_
     return V
 
-def policy_iteration(policy_prob, V, g):
-    old_pp = policy_prob
+def policy_iteration(policy, g, gamma, delta):
+    old_pp = policy
     while True:
+        V = policy_evaluation(old_pp, g, gamma, delta)
         policychange = False
         new_pp = {}
         for s in g.all_states():
-            qmax, amax = 0, 0
-            actions = g.actions.get(s, ())
-            for a in actions:
-                qn = float('-inf')
+            qmax, amax = float('-inf'), 0
+            old_a = old_pp.get(s, 0)
+            for a in g.actions.get(s, ()):
+                qn = 0
                 probs = g.probs.get((s,a), {})
                 for s_ in list(probs.keys()):
                     r = g.rewards.get(s_, 0)
@@ -59,37 +59,34 @@ def policy_iteration(policy_prob, V, g):
                 if qn >= qmax:
                     qmax = qn
                     amax = a
-            new_pp[s] = {a: 1.0}
-            if new_pp[s] != old_pp.get(s, {}): 
+            new_pp[s] = amax
+            if new_pp[s] != old_pp.get(s, 0): 
                 policychange = True
         if policychange == False: break
         old_pp = new_pp
+    return new_pp
 
-    return policy_prob
-
-
+def policy_initialization(g):
+    policy = {}
+    for s in g.actions.keys():
+        policy[s] = np.random.choice(g.actions[s])
+    return policy
 
 
 if __name__ == '__main__':
     g = gw.windy_grid()
-    g = gw.windy_grid_penalized(g, -0.1)
+    g = gw.windy_grid_penalized(g, -0.4)
     # 1(0001) for Right, 2(0010) for Left, 4(0100) for Down, 8(1000) for Up
-    policy_prob = {
-    (2, 0): {8: 0.5, 1: 0.5},
-    (1, 0): {8: 1.0},
-    (0, 0): {1: 1.0},
-    (0, 1): {1: 1.0},
-    (0, 2): {1: 1.0},
-    (1, 2): {8: 1.0},
-    (2, 1): {1: 1.0},
-    (2, 2): {8: 1.0},
-    (2, 3): {2: 1.0},
-    }
     delta = 1e-3
     gamma = 0.9
 
-    V = policy_evaluation(policy_prob, g, gamma, delta)
-    P = policy_iteration(policy_prob, V, g)
+    policy = policy_initialization(g)
+    print('Initial:')
+    print_policy(policy, g)
+    print_value(g.rewards, g)
+
+    P = policy_iteration(policy, g, gamma, delta)
+    V = policy_evaluation(P, g, gamma, delta)
 
     print('After iteration:')
     print_policy(P, g)
